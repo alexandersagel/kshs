53fe9e4ef2057de3515a046b666a77739e7597ed
Alexander Sagel <a.sagel@tum.de>
2020-09-09 19:56:38 +0200

    download script

--- download_scattering_histograms.py
+++ download_scattering_histograms.py
@@ -8,17 +8,29 @@ Created on Wed Sep  9 18:54:54 2020
 
 import urllib.request
 import zipfile
+import os
+import shutil
 
 print('Downloading scattering_histograms.zip...')
 url = 'https://www.dropbox.com/s/7m3tmohqyi9fod1/scattering_histograms.zip?dl=1'
 urllib.request.urlretrieve(url, './data/scattering_histograms.zip')
 print('Unzipping...')
 zf = zipfile.ZipFile('./data/scattering_histograms.zip')
-zf.extractall('./data/')
+for f in zf.namelist():
+    if f.endswith('npy'):
+        zf.extract(f, 'data/')
+        p = f.split('/')
+        os.rename('data/'+f, 'data/'+p[1]+'/'+p[2] + '/' + '/' + p[3])
+shutil.rmtree('data/'+p[0])
 print('Downloading scattering_histograms_normalized.zip...')
 url = 'https://www.dropbox.com/s/dquj7cd368wmzzl/scattering_histograms_normalized.zip?dl=1'
 urllib.request.urlretrieve(url, './data/scattering_histograms_normalized.zip')
 print('Unzipping...')
 zf = zipfile.ZipFile('./data/scattering_histograms_normalized.zip')
-zf.extractall('./data/')
+for f in zf.namelist():
+    if f.endswith('npy'):
+        zf.extract(f, 'data/')
+        p = f.split('/')
+        os.rename('data/'+f, 'data/'+p[1]+'/'+p[2] + '/' + '/' + p[3])
+shutil.rmtree('data/'+p[0])
 print('Done!')
